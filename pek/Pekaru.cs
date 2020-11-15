0c1b9b3b1315186c88aff7c731131b0d8f184f0a
telea <tucker.lea27@gmail.com>
2020-11-15 17:54:13 +0100

    pekaru

---
+++ pek/Pekaru.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pek
+{
+    class Pekaru
+    {
+        string nev;
+        int ar;
+        bool laktozmentes;
+
+        public Pekaru(string nev, int ar, bool laktozmentes)
+        {
+            this.Nev = nev;
+            this.Ar = ar;
+            this.Laktozmentes = laktozmentes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Ar { get => ar; set => ar = value; }
+        public bool Laktozmentes { get => laktozmentes; set => laktozmentes = value; }
+
+        public override string ToString()
+        {
+            string s = nev+"("+ar+"Ft/db)";
+            return s;
+        }
+    }
+}
